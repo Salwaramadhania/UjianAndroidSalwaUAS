a31248f2c5394bfe4ec56858f908acdd93de2614
Salwaramadhania <salwaramadhania123@gmail.com>
2024-01-10 21:24:36 +0700

    Update UAS

--- app/src/main/java/com/example/ujianandroid/ListActivity.java
+++ app/src/main/java/com/example/ujianandroid/ListActivity.java
@@ -1,33 +1,54 @@
-package com.example.ujianandroid;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ujianandroid.R;
 
 import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
+
+    private ListView lvNama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        ListView lvNama = (ListView) findViewById(R.id.lvNama);
+        lvNama = findViewById(R.id.lvNama);
 
-        ArrayList<String> daftar_nama = getIntent().getExtras().getStringArrayList("daftar_nama");
+        ArrayList<String> daftar_nama = getIntent().getStringArrayListExtra("daftar_nama");
+        ArrayList<String> filteredList = new ArrayList<>();
 
-        if(daftar_nama.isEmpty()){
-            daftar_nama.add("data masih kosong");
+        if (daftar_nama != null && !daftar_nama.isEmpty()) {
+            for (String nama : daftar_nama) {
+                // Mengekstrak nomor urut, nama depan, nama belakang, dan usia
+                String[] parts = nama.split("\\. ");
+                int index = Integer.parseInt(parts[0]);
+                String[] nameAndAge = parts[1].split(" - Status: ");
+                String name = nameAndAge[0];
+                String status = nameAndAge[1];
+
+                // Menampilkan nama depan, nama belakang, dan status dalam format yang diinginkan
+                String[] nameParts = name.split(" ");
+                if (nameParts.length >= 2) {
+                    String namaLengkap = nameParts[0] + " " + nameParts[1];
+                    filteredList.add(index + ". " + namaLengkap + " - Status: " + status);
+                }
+            }
         }
 
-        ArrayAdapter<String> ad_nama = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, daftar_nama);
+        ArrayAdapter<String> ad_nama;
+        if (!filteredList.isEmpty()) {
+            ad_nama = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, filteredList);
+        } else {
+            filteredList.add("Data masih kosong");
+            ad_nama = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, filteredList);
+        }
 
         lvNama.setAdapter(ad_nama);
-
     }
 }
